686ad830fdbd899caaba95bf05f1178f9421783a
Alex Fallenstedt and Mary Warrington <a.fallenstedt@gmail.com>
2016-01-19 08:50:55 -0800

    Add js to return true on years divisible by 4

--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('leapYear', function() {
   it("is false for a year that is not divisible by 4, 100 or 400", function() {
     expect(leapYear(1993)).to.equal(false);
   });
+  it("is true for a year that is divisible by 4", function() {
+    expect(leapYear(1988)).to.equal(true);
+  });
 });
